7b27ef795d4b3f7f7c78bd57909a35c031b059f4
shyam0705 <72148710+shyam0705@users.noreply.github.com>
2020-09-30 22:34:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 import BubbleMain from './components/BubbleMainComponent.js';
 import SelectionMain from './components/SelectionMainComponent.js';
 import Main from './components/MainComponent';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Main />
-        </BrowserRouter>
+        </HashRouter>
   );
 }
 
